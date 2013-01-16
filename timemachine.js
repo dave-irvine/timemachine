fbf411b93957874bf2eda482fa7abf8c0627dcd9
Dave Irvine <dave@daveirvine.me>
2013-01-16 18:40:43 +0000

    If commit time ranges are specified, convert them into Date objects

--- timemachine.js
+++ timemachine.js
@@ -6,6 +6,11 @@ var options = {
 }, params = [];
 
 convertArgvToOptionsAndParams(process.argv.slice(2), options, params);
+
+if (options.validCommitTimes !== null) {
+	options.validCommitTimes = parseCommitTimes(options.validCommitTimes);
+}
+
 function convertArgvToOptionsAndParams(argv, options, params) {
 	var dashRegex = /^-+([^=]+)?=?(.+)/,
 		options = options || {},
@@ -37,4 +42,29 @@ function convertArgvToOptionsAndParams(argv, options, params) {
 			params.push(arg);
 		}
 	});
+}
+
+function parseCommitTimes(commitTimes) {
+	var timeRanges = [];
+
+	commitTimes = commitTimes.split(",");
+
+	commitTimes.forEach(function (timeRange) {
+		var ranges = timeRange.split(":"),
+			range = {};
+
+		range.start = new Date();
+		range.start.setHours(ranges[0].substr(0, 2));
+		range.start.setMinutes(ranges[0].substr(2, 2));
+		range.start.setSeconds(0);
+
+		range.end = new Date();
+		range.end.setHours(ranges[1].substr(0, 2));
+		range.end.setMinutes(ranges[1].substr(2, 2));
+		range.end.setSeconds(0);
+
+		timeRanges.push(range);
+	});
+
+	return timeRanges;
 }
