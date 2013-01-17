bf130f2c7c675c9c7a5d7da203b6ca514f4d7e64
Dave Irvine <dave@daveirvine.me>
2013-01-17 13:32:13 +0000

    Refactor timemachine to use clu

--- timemachine.js
+++ timemachine.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var Git = require("./lib/git"),
 	path = require("path"),
+	clu = require("./lib/clu"),
 	repoPath = "",
 	repo = null,
 	options = {
@@ -16,7 +17,7 @@ process.on('uncaughtException', function (exception) {
 
 if (process.argv.length < 4) { throw new Error("TimeMachine :: Not enough options/parameters supplied."); }
 
-convertArgvToOptionsAndParams(process.argv.slice(2), options, params);
+clu.convertArgvToOptionsAndParams(process.argv.slice(2), options, params);
 
 if (options.validCommitTimes !== null) {
 	options.validCommitTimes = parseCommitTimes(options.validCommitTimes);
@@ -29,38 +30,6 @@ try {
 } catch(e) {
 	throw new Error("TimeMachine :: Couldn't find git repository. Path supplied was: " + params[0]);
 }
-function convertArgvToOptionsAndParams(argv, options, params) {
-	var dashRegex = /^(-+)([^=]+)?=?(.+)/,
-		options = options || {},
-		params = params || [];
-
-	argv.forEach(function (arg) {
-		var dashCount = 0,
-			dashRegexMatches = [],
-			argName,
-			argValue;
-
-		if (dashRegex.test(arg)) {
-			dashRegexMatches = dashRegex.exec(arg);
-			dashCount = dashRegexMatches[1].split("-").length - 1;
-			argName = dashRegexMatches[2];
-			argValue = dashRegexMatches[3];
-		}
-		
-		if (dashCount === 1) {
-			if (!argName) { argName = argValue; }
-
-			options[argName] = true;
-		} else if (dashCount === 2) {
-			if (options[argName] === undefined) {
-				throw new Error("TimeMachine :: Unknown option near: " + arg);
-			} else if (argValue === "=") {
-				throw new Error("TimeMachine :: Empty option assignment near: " + arg);
-			}
-			
-			options[argName] = argValue;
-		} else {
-			params.push(arg);
 		}
 	});
 }
