4a6fc49feaf8698ddb7be52c8c42afc9e5559447
Dave Irvine <dave@daveirvine.me>
2013-03-17 23:19:46 +0000

    Create a test repository with a commit.

--- test/testbed/setup.js
+++ test/testbed/setup.js
@@ -19,9 +19,44 @@ var create_git_repo = function (path, bare) {
 	return clu.exec(".", "git init", [args, path]).then(function (stdout) { console.log(stdout); });
 };
 
+var create_git_commit = function (path, empty, message) {
+	var deferred = Q.defer();
+	if (!empty) { empty = false; }
+	if (!message) { message = false; }
+
+	var do_commit = function () {
+		var opts = {
+			"allow-empty": empty,
+		}
+
+		if (message) {
+			opts["m"] = "'" + message + "'";
+		} else {
+			opts["allow-empty-message"] = true;
+		}
+
+		var args = clu.convertOptionsToArgv(opts);
+
+		clu.exec(path, "git commit", [args]).then(
+			function () {
+				deferred.resolve();
+			},
+			function (err) {
+				deferred.reject();
+			}
+		);
+	};
+
+	do_commit();
+
+	return deferred.promise;
+};
+
 var sequence = [
 	function () { return create_git_repo("test/testbed/fixtures/simple"); },
-	function () { return create_git_repo("test/testbed/fixtures/bare", true); }
+	function () { return create_git_repo("test/testbed/fixtures/bare", true); },
+	function () { return create_git_repo("test/testbed/fixtures/withLogs"); },
+	function () { return create_git_commit("test/testbed/fixtures/withLogs", true, "1st commit."); }
 ];
 
 var result = Q.resolve(create_directory("test/testbed/fixtures"));
