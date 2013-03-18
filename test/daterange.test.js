62df58066715f1387133601e9719b55e4b5de09a
Dave Irvine <dave@daveirvine.me>
2013-03-18 23:28:00 +0000

    Added containsDate() and normaliseDateTime() functions to DateRange.
    normaliseDateTime() allows you to normalise the Time portion of a Date
    to either the first second of a Date, the last second of a Date, or
    midday on a Date.

--- test/daterange.test.js
+++ test/daterange.test.js
@@ -5,7 +5,6 @@ var chai = require("chai"),
 	DateRange = require("../lib/daterange"),
 	expect = chai.expect;
 
-
 describe("DateRange", function () {
 	describe("constructor", function () {
 		var dateRange;
@@ -56,6 +55,65 @@ describe("DateRange", function () {
 			expect(function () {
 				dateRange = new DateRange("Thu Jan 01 1970 00:00:00 GMT+0000 (GMT)~Fri Jan 02 1970 00:00:00 GMT+0000 (GMT)");
 			}).to.not.throws();
+		});
+	});
+
+	describe("containsDate()", function () {
+		var containsDate,
+			dateRange,
+			testDate;
+
+		it("should throw an Error if passed an invalid date", function () {
+			expect(function () {
+				dateRange.containsDate("abcd1234");
+			}).to.throws();
+		});
+
+		it("should return true if it contains the passed date", function () {
+			testDate = new Date("Fri Jan 02 1970 00:00:00 GMT+0000 (GMT)");
+			dateRange = new DateRange("Thu Jan 01 1970 00:00:00 GMT+0000 (GMT)~Sat Jan 03 1970 00:00:00 GMT+0000 (GMT)");
+
+			containsDate = dateRange.containsDate(testDate);
+
+			expect(containsDate).to.be.true;
+		});
+
+		it("should return false if it does not contain the passed date", function () {
+			testDate = new Date("Sun Jan 04 1970 00:00:00 GMT+0000 (GMT)");
+			dateRange = new DateRange("Thu Jan 01 1970 00:00:00 GMT+0000 (GMT)~Sat Jan 03 1970 00:00:00 GMT+0000 (GMT)");
+
+			containsDate = dateRange.containsDate(testDate);
+
+			expect(containsDate).to.be.false;
+		});
+	});
+
+	describe("normaliseDateTime()", function () {
+		var normalisedDate,
+			testDate = new Date("Fri Jan 02 1970 00:00:00 GMT+0000 (GMT)");
+
+		it("should normalise a Date's time to 00:00:01 if passed 'toStart' as true in options", function () {
+			normalisedDate = DateRange.normaliseDateTime(testDate, { toStart: true });
+
+			expect(normalisedDate.getHours()).to.equal(0);
+			expect(normalisedDate.getMinutes()).to.equal(0);
+			expect(normalisedDate.getSeconds()).to.equal(1);
+		});
+
+		it("should normalise a Date's time to 23:59:59 if passed 'toEnd' as true in options", function () {
+			normalisedDate = DateRange.normaliseDateTime(testDate, { toEnd: true });
+
+			expect(normalisedDate.getHours()).to.equal(23);
+			expect(normalisedDate.getMinutes()).to.equal(59);
+			expect(normalisedDate.getSeconds()).to.equal(59);
+		});
+
+		it("should normalise a Date's time to 12:00:00 if passed 'toStart' and toEnd' as true in options", function () {
+			normalisedDate = DateRange.normaliseDateTime(testDate, { toStart: true, toEnd: true });
+
+			expect(normalisedDate.getHours()).to.equal(12);
+			expect(normalisedDate.getMinutes()).to.equal(0);
+			expect(normalisedDate.getSeconds()).to.equal(0);
 		});
 	});
 });
